5734e621ffa3e9e69cb269f212ce614afae89ff5
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2021-11-18 02:06:18 -0500

    CREA LA BASE DE DATOS

---
+++ Crear_Base_Datos_POSTGRESQL_PAQUETES_TURISTICOS.sql
@@ -0,0 +1,13 @@
+-- Database: venta_paquete_turistico
+
+-- DROP DATABASE venta_paquete_turistico;
+
+CREATE DATABASE venta_paquete_turistico
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
